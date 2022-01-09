ae14a0ea32570f2ae8296265925bd9b8264490d9
murilonerdx <mu-silva@outlook.com>
2022-01-09 16:52:55 -0300

    Lendo diferentes dados, fazendo tratamento e tipo de corpo

--- src/com/murilonerdx/Main.java
+++ src/com/murilonerdx/Main.java
@@ -20,14 +20,14 @@ public class Main {
 
         lerObjetoItem();
 
+        pegarMoeda();
+
+        lerTodosDados();
 
     }
 
     public static void lerUmItem() throws IOException, SAXException, ParserConfigurationException {
-        DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = fabrica.newDocumentBuilder();
-
-        Document document = builder.parse("src/com/murilonerdx/vendas.xml");
+        Document document = factoryBuilder().parse("src/com/murilonerdx/vendas.xml");
         NodeList formaDePagamentos = document.getElementsByTagName("formaDePagamento");
         Element item = (Element) formaDePagamentos.item(0);
 
@@ -37,9 +37,7 @@ public class Main {
     }
 
     public static void lerMuitosItems() throws IOException, SAXException, ParserConfigurationException {
-        DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = fabrica.newDocumentBuilder();
-        Document document = builder.parse("src/com/murilonerdx/vendas.xml");
+        Document document = factoryBuilder().parse("src/com/murilonerdx/vendas.xml");
 
         NodeList produtos = document.getElementsByTagName("nome");
 
@@ -52,10 +50,7 @@ public class Main {
     }
 
     public static void lerObjetoItem() throws IOException, SAXException, ParserConfigurationException{
-        DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = fabrica.newDocumentBuilder();
-
-        Document document = builder.parse("src/com/murilonerdx/vendas.xml");
+        Document document = factoryBuilder().parse("src/com/murilonerdx/vendas.xml");
         NodeList produtos = document.getElementsByTagName("produto");
 
         for(int i = 0;i < produtos.getLength();i++) {
@@ -66,5 +61,35 @@ public class Main {
 
             System.out.println(prod);
         }
+    }
+
+    public static void pegarMoeda() throws IOException, SAXException, ParserConfigurationException {
+        Document document = factoryBuilder().parse("src/com/murilonerdx/vendas.xml");
+
+        Element venda = document.getDocumentElement();
+        String moeda = venda.getAttribute("moeda");
+        System.out.println(moeda);
+    }
+
+    public static void lerTodosDados() throws ParserConfigurationException, IOException, SAXException {
+        Document document = factoryBuilder().parse("src/com/murilonerdx/vendas.xml");
+
+        NodeList produtos = document.getElementsByTagName("produto");
+
+        for(int i =0; i < produtos.getLength(); i++) {
+            Element produto = (Element) produtos.item(i);
+            String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+            String preco = produto.getElementsByTagName("preco").item(0).getTextContent();
+
+            System.out.println("-----------");
+            System.out.println("Nome do produto: "+ nome);
+            System.out.println("Preco do produto: "+ preco);
+            System.out.println("-----------");
+        }
+    }
+
+    public static DocumentBuilder factoryBuilder() throws ParserConfigurationException {
+        DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
+        return fabrica.newDocumentBuilder();
     }
 }
